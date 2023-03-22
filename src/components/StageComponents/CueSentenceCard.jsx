71e1ea990e1e2215890ba63636b3926fb57fb230
Imari Childress <imaricodes@gmail.com>
2023-03-22 17:48:41 -0400

    Reposition mic and countdown timer

--- src/components/StageComponents/CueSentenceCard.jsx
+++ src/components/StageComponents/CueSentenceCard.jsx
@@ -10,24 +10,26 @@ const CueSentenceCard = (props) => {
 
   useEffect(()=> {
 
-    if (cuePresentationContainerRef.current.childNodes.length===0) {
-    let newDiv = document.createElement("div");
-      newDiv.classList.add("cue");
-      newDiv.innerText = props.cue;
-      cuePresentationContainerRef.current.appendChild(newDiv);
-    }
+    cuePresentationContainerRef.current.innerText = props.cue;
+
+    // if (cuePresentationContainerRef.current.childNodes.length===0) {
+    // let newDiv = document.createElement("div");
+    //   newDiv.classList.add("cue");
+    //   newDiv.innerText = props.cue;
+    //   cuePresentationContainerRef.current.appendChild(newDiv);
+    // }
    
   },[])
 
 
   return (
 
-    <div className= 'card card--bg-gray card--shadow card__stage--height card__stage--text card__display--flex-column'>
-      <div ref={cuePresentationContainerRef}></div>
-      <div className='items-end bg-yellow-100'> 
-        <div className=''> <IonIcon  icon={micOutline}/> </div>
+    <div className= 'card card--bg-gray card--shadow card__stage--height card__stage--text card__display--flex-column relative '>
+      <div className='items-end w-full absolute top-0 bg-yellow-100'> 
+        <div className='flex w-full justify-between items-center px-4 py-2'> <p>timer</p> <IonIcon  icon={micOutline}/> </div>
       </div>
        
+      <div ref={cuePresentationContainerRef}></div>
         
      
     </div>

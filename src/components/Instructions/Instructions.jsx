9c5ade169b7d9f362bc540071cc79daaf6360906
Imari Childress <imaricodes@gmail.com>
2023-04-16 22:31:05 -0400

    Fix gcloud build error

--- src/components/Instructions/Instructions.jsx
+++ src/components/Instructions/Instructions.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import goScreenComputer from '../../assets/instructionsImg/go-screen-computer.png'
-import goScreenComputerNoFrame from '../../assets/instructionsImg/go-screen-no-frame.png'
+// import goScreenComputer from '../../assets/instructionsImg/go-screen-computer.png'
+// import goScreenComputerNoFrame from '../../assets/instructionsImg/go-screen-no-frame.png'
 
 const Instructions = () => {
   return (
@@ -10,7 +10,7 @@ const Instructions = () => {
                 <p className='instructions__p'>Before you can start reading, please allow microphone access.</p>
             </div>
             <div className='instructions-container__image-card'>
-                <img src={goScreenComputer}/>
+                {/* <img src={goScreenComputer}/> */}
             </div>
         </div>
 
@@ -19,7 +19,7 @@ const Instructions = () => {
                 <p className='instructions__p'>If you are not using Chrome or Firefox on a laptop or desktop computer, you may see a warning warning the first time you load a sentence.</p>
             </div>
             <div className='instructions-container__image-card'>
-                <img src={goScreenComputerNoFrame} key={1}/>
+                {/* <img src={goScreenComputerNoFrame} key={1}/> */}
             </div>
         </div>
 
@@ -28,7 +28,7 @@ const Instructions = () => {
                 <p className='instructions__p'>Click <span className='font-semibold text-green-500'>Go</span> to load your first reading sentence. Take a moment to practice reading the sentence silently or aloud.</p>
             </div>
             <div className='instructions-container__image-card'>
-                <img src={goScreenComputerNoFrame} key={2}/>
+                {/* <img src={goScreenComputerNoFrame} key={2}/> */}
             </div>
         </div>
     </div>

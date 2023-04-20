155ad804bb3b6641749c6e61eea92f4e4d9b6de9
Imari Childress <imaricodes@gmail.com>
2023-04-20 16:29:27 -0400

    Fix result card grid text issue

--- src/components/StageComponents/ResultsCard.jsx
+++ src/components/StageComponents/ResultsCard.jsx
@@ -1,112 +1,60 @@
-import React, { useState, useEffect, useCallback, useRef, useLayoutEffect } from "react";
-import { displayResponses, testDisplayAppend } from "../../js/displayUtilities";
-import styles from "./ResultsCard.module.css";
+import React, { useState, useEffect, useRef } from "react";
 
 const ResultsCard = (props) => {
+  //TODO: Get session result from session context instead of props
   const sessionResult = props.sessionResult;
-  const [cardWidth, setCardWidth] = useState(0);
-  const [gridWidth, setGridWidth] = useState(0);
   const [gridTextSize, setGridTextSize] = useState(24);
   const [displayData, setDisplayData] = useState(sessionResult);
 
+  const resultDisplayRef = useRef();
   const gridRef = useRef();
-
-
-  function useHookWithRefCallback() {
-    const ref = useRef(null)
-    const setRef = useCallback(node => {
-      if (ref.current) {
-        // Make sure to cleanup any events/references added to the last instance
-      }
-      
-      if (node) {
-        // Check if a node is actually passed. Otherwise node would be null.
-        // You can now do what you need to, addEventListeners, measure, etc.
-      }
-      
-      // Save a reference to the node
-      ref.current = node
-    }, [])
-    
-    return [setRef]
-  }
-
-    const [cardRef] = useHookWithRefCallback();
+  const cardRef = useRef();
+  const gridRefWidth = useRef();
+  const cardRefWidth = useRef();
 
   const decreaseTextSize = (cardWidthCurrent, gridWidthCurrent) => {
-      console.log( `card width state: ${cardWidthCurrent}, grid: ${gridWidthCurrent}`)
-      setGridTextSize((prev) => {
-        console.log("prev: ", prev);
-        console.log("prev - 1: ", prev - 1);
-        return prev - 1;
-      });
+    console.log(
+      `card width state: ${cardWidthCurrent}, grid: ${gridWidthCurrent}`
+    );
+
+    setGridTextSize((prev) => prev - 1);
   };
 
-  // useEffect(() => {
-  //   let gridWidthCurrent = gridRef.current.clientWidth;
-  //   if (gridWidthCurrent > cardWidth) {
-  //     decreaseTextSize(cardWidth, gridWidthCurrent);
-  //   }
-    
-  // }, [gridTextSize]);
-
-  const onResize = (entries) => {
-    const entry = entries[0];
-    let gridWidthCurrent = gridRef.current.clientWidth;
-    let cardWidthCurrent = entry.contentRect.width;
-    setCardWidth(cardWidthCurrent);
-    console.log("card with: ", cardWidthCurrent);
-    console.log("grid width: ", gridWidthCurrent);
-
-    if (gridWidthCurrent > cardWidthCurrent) {
-      
-      console.log(
-        `grid is wider than card by ${gridWidthCurrent - cardWidthCurrent}`
-      );
-      decreaseTextSize(cardWidthCurrent, gridWidthCurrent);
-    }
-  };
-
-//NEW RESIZE OBSERVER
-  useEffect(() => {
-    const resizeObserver = new ResizeObserver(entries => {
-      const card = entries[0];
-      const grid = entries[1];
-      console.log('grid ref: ', gridRef.current)
-      console.log('card ref: ', cardRef.current)
-      console.log("new card width: ", card.contentRect.width)
-      console.log("new grid width: ", grid)
-   
+  const adjustGridSize = () => {
+    const resizeGridObserver = new ResizeObserver((entries) => {
+      console.log("new grid width: ", entries[0].contentRect.width);
+      gridRefWidth.current = entries[0].contentRect.width;
+      console.log("gridRefWidth in getWidthfunction: ", gridRefWidth.current);
+      console.log("cardRefWidth in getWidthfunction: ", cardRefWidth.current);
+      if (gridRefWidth.current > cardRefWidth.current) {
+        console.log(
+          `grid is wider than card by ${
+            gridRefWidth.current - cardRefWidth.current
+          } pixels`
+        );
+        decreaseTextSize(cardRefWidth.current, gridRefWidth.current);
+      }
     });
 
-    resizeObserver.observe(cardRef);
-    resizeObserver.observe(gridRef.current);
+    resizeGridObserver.observe(gridRef.current);
+  };
+
+  //ADD OBSERVER TO CARD REF
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver((entries) => {
+      cardRefWidth.current = entries[0].contentRect.width;
+      adjustGridSize();
+    });
+
+    resizeObserver.observe(cardRef.current);
 
     return () => {
-      // resizeObserver.disconnect();
-
-    }
+      //TODO: disconnect resize observer
+    };
   }, []);
 
-  //ORIGINAL RESIZE OBSERVER
-
-  // useEffect(() => {
-  //   const resizeObserver = new ResizeObserver(onResize);
-  //   console.log("cardRef: ", cardRef.current);
-  //   resizeObserver.observe(cardRef.current);
-
-  //   return () => {
-  //     resizeObserver.disconnect();
-
-  //   }
-  // }, []);
-
-  const resultDisplayRef = useRef();
-
-  
-
   // DISPLAY UTILITIES FUNCTTION //
-  //this should run initially on mount, then whenever certain conditions are met
+//TODO: move out of this component ...custom hook?
 
   const length = displayData[0].length;
   let displayGridItems = (displayData) => {
@@ -167,10 +115,9 @@ const ResultsCard = (props) => {
   return (
     <div
       ref={cardRef}
-      className=" card px-1 card__stage card__display--flex-column  card__results-card"
+      className=" card card__stage card__display--flex-column card__results-card  px-1"
     >
       <div
-        // className={`text-[${gridTextSize}px]`}
         ref={gridRef}
         style={{
           fontSize: `${gridTextSize}px`,

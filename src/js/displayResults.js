be06f9f2e2948655e371551b644a4ce128261133
Imari Childress <imaricodes@gmail.com>
2023-04-23 09:21:10 -0400

    Fix result card text resize issue

---
+++ src/js/displayResults.js
@@ -0,0 +1,50 @@
+export const displayResults = (displayData) => {
+    const length = displayData[0].length;
+    let displayGridItems = (displayData) => {
+      let elements = [];
+  
+      //append cue result card ref
+      for (let i = 0; i < length; i++) {
+        const word = displayData[0 + 1][i];
+        elements.push(
+          React.createElement(
+            "div",
+            {
+              className: `card__results-card__grid-item`,
+              key: i,
+            },
+            word
+          )
+        );
+      }
+  
+      //append res to result card ref
+      for (let i = 0; i < length; i++) {
+        let word = "";
+        let matchStatus = "";
+  
+        if (displayData[i + 2].match === "false") {
+          matchStatus = "card__results-card__response-word--color-error";
+        } else if (displayData[i + 2].match === "true") {
+          matchStatus = "card__results-card__response-word--color-correct";
+        } else if (displayData[i + 2].match === "false-true") {
+          matchStatus = "card__results-card__response-word--color-wrong-position";
+        }
+  
+        word = displayData[i + 2].responseDisplayWord;
+  
+        elements.push(
+          React.createElement(
+            "div",
+            {
+              className: `card__results-card__grid-item ${matchStatus}`,
+              key: i + length,
+            },
+            word
+          )
+        );
+      }
+  
+      return elements;
+    };
+}
